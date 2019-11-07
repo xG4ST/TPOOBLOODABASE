f3dfd8370baeba2bce36d97d715108c0fb4bb2b3
Gustavo Adolfo Sánchez Torres <gus64_gast@outlook.es>
2019-11-07 11:08:24 -0600

    Cesar

--- LOGIN/LOGIN/Login.Designer.cs
+++ LOGIN/LOGIN/Login.Designer.cs
@@ -30,8 +30,6 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.bunifuMaterialTextbox1 = new Bunifu.Framework.UI.BunifuMaterialTextbox();
-            this.Textbox2 = new Bunifu.Framework.UI.BunifuMaterialTextbox();
             this.bunifuImageButton2 = new Bunifu.Framework.UI.BunifuImageButton();
             this.bunifuImageButton1 = new Bunifu.Framework.UI.BunifuImageButton();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
@@ -59,44 +57,6 @@
             this.label2.Size = new System.Drawing.Size(147, 30);
             this.label2.TabIndex = 6;
             this.label2.Text = "CONTRASEÑA";
-            // 
-            // bunifuMaterialTextbox1
-            // 
-            this.bunifuMaterialTextbox1.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.bunifuMaterialTextbox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
-            this.bunifuMaterialTextbox1.ForeColor = System.Drawing.Color.White;
-            this.bunifuMaterialTextbox1.HintForeColor = System.Drawing.Color.White;
-            this.bunifuMaterialTextbox1.HintText = "";
-            this.bunifuMaterialTextbox1.isPassword = false;
-            this.bunifuMaterialTextbox1.LineFocusedColor = System.Drawing.Color.Transparent;
-            this.bunifuMaterialTextbox1.LineIdleColor = System.Drawing.Color.Silver;
-            this.bunifuMaterialTextbox1.LineMouseHoverColor = System.Drawing.Color.DodgerBlue;
-            this.bunifuMaterialTextbox1.LineThickness = 3;
-            this.bunifuMaterialTextbox1.Location = new System.Drawing.Point(155, 414);
-            this.bunifuMaterialTextbox1.Margin = new System.Windows.Forms.Padding(4);
-            this.bunifuMaterialTextbox1.Name = "bunifuMaterialTextbox1";
-            this.bunifuMaterialTextbox1.Size = new System.Drawing.Size(364, 33);
-            this.bunifuMaterialTextbox1.TabIndex = 7;
-            this.bunifuMaterialTextbox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            // 
-            // Textbox2
-            // 
-            this.Textbox2.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.Textbox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
-            this.Textbox2.ForeColor = System.Drawing.Color.White;
-            this.Textbox2.HintForeColor = System.Drawing.Color.White;
-            this.Textbox2.HintText = "";
-            this.Textbox2.isPassword = false;
-            this.Textbox2.LineFocusedColor = System.Drawing.Color.Transparent;
-            this.Textbox2.LineIdleColor = System.Drawing.Color.Silver;
-            this.Textbox2.LineMouseHoverColor = System.Drawing.Color.DodgerBlue;
-            this.Textbox2.LineThickness = 3;
-            this.Textbox2.Location = new System.Drawing.Point(155, 552);
-            this.Textbox2.Margin = new System.Windows.Forms.Padding(4);
-            this.Textbox2.Name = "Textbox2";
-            this.Textbox2.Size = new System.Drawing.Size(364, 33);
-            this.Textbox2.TabIndex = 8;
-            this.Textbox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
             // 
             // bunifuImageButton2
             // 
@@ -140,8 +100,6 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(700, 900);
-            this.Controls.Add(this.Textbox2);
-            this.Controls.Add(this.bunifuMaterialTextbox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.bunifuImageButton2);
@@ -167,8 +125,6 @@
         private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private Bunifu.Framework.UI.BunifuMaterialTextbox bunifuMaterialTextbox1;
-        private Bunifu.Framework.UI.BunifuMaterialTextbox Textbox2;
     }
 }
 
