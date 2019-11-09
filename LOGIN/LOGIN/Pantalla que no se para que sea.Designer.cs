2c86168ab9f75ad32f4d0c54f1dd9cd91fd2d0f6
Gustavo Adolfo Sánchez Torres <gus64_gast@outlook.es>
2019-11-09 16:20:34 -0600

    boton sincronizado

--- LOGIN/LOGIN/Pantalla que no se para que sea.Designer.cs
+++ LOGIN/LOGIN/Pantalla que no se para que sea.Designer.cs
@@ -44,7 +44,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(132, 164);
+            this.label2.Location = new System.Drawing.Point(194, 211);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(439, 13);
             this.label2.TabIndex = 1;
