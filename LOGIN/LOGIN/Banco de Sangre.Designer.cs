e7efeb07ddaa95b301ad626f6cf7c748457b785d
Gabs2000 <cesardz_2000_00@outlook.com>
2019-11-09 23:25:11 -0600

    Tratando de arreglar

--- LOGIN/LOGIN/Banco de Sangre.Designer.cs
+++ LOGIN/LOGIN/Banco de Sangre.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace LOGIN
 {
-    partial class inicio
+    partial class Inicio
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(inicio));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inicio));
             this.panel3 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
